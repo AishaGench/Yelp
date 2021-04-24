146237eb05a9428ff553e81dfe73ba09c6275c48
Aisha Gench <aisha.gench@gmail.com>
2021-04-23 21:18:02 -0500

    created search bar js and css

--- apollo/src/components/SearchBar/SearchBar.js
+++ apollo/src/components/SearchBar/SearchBar.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react'
-
+import "./SearchBar.css"
 
 export default class SearchBar extends Component {
-    sortByOptions = {
-        'Best Match':'best_match',
-        'Highest Rated':'highest_rated',
-        'Most Reviewed': 'most_reviewed'
-    }
+    
+        sortByOptions={
+            "Best Match": "best_match",
+            "Highest Rated": "raiting",
+            "Most Reviewed": "review_count"
+        }
+        renderSortByOptions(){
+            return Object.keys(this.sortByOptions).map(sortByOption=>{
+                    const sortByOptionValue=this.sortByOptions[sortByOption]
+                    return <li key={sortByOptionValue}>{sortByOption}</li>
+            })
+        }
+    
+   
     render() {
         return (
             <div className="SearchBar">
-        <div className="SearchBar-sort-options">
-          <ul>
-            {this.renderSortByOptions()}
-          </ul>
-        </div>
-        <div className="SearchBar-fields">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
           <input placeholder="Search Businesses" />
           <input placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
-          <a href='www.#.com' >Let's Go</a>
+          <a href='www.#.com'>Let's Go</a>
         </div>
       </div>
 
