37c4a1b8dacdeefaa3b6ec70dbdace6455cf0660
Aisha Gench <aisha.gench@gmail.com>
2021-04-23 21:11:53 -0500

    created App component

--- apollo/src/components/App/App.js
+++ apollo/src/components/App/App.js
@@ -1,10 +1,16 @@
-
+import React from "react";
+import './App.css';
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
 
 function App() {
-    return(
-        <div>
-
-        </div>
-    )
-    
+  return (
+    <div className="App">
+      <SearchBar/>
+      <BusinessList/>
+      
+    </div>
+  );
 }
+
+export default App;
